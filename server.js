c84eac99f5ddc555fa0ece58598b5ba1ba3a8e8b
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-03-19 17:49:45 +0300

    routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config()
 
 const app = express();
 
+app.use('/api/contacts', require('./routes/contactRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
