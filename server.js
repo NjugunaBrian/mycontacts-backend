0ab9f2e9f35de834e290858797bcb2a4a8203352
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-03-20 18:00:21 +0300

    errorHandlers

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config()
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
