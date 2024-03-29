81da56f8cf1e8281a6b0f51e786b47b5944ea19b
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-03-29 19:27:20 +0300

    mongoose connection

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config()
 
 const app = express();
 
-
+connectDb();
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use(errorHandler);
